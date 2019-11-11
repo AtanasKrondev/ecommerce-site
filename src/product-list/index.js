6c2ccac7ea1077af9a2cd49bee6b96c81c643058
Atanas Krondev <krondev@gmail.com>
2019-11-11 10:06:15 +0200

    Styles changed

--- src/product-list/index.js
+++ src/product-list/index.js
@@ -1,29 +1,29 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
+// import styled, { css } from 'styled-components'
 
 import ProductCard from '../product-card'
 import Header from '../header'
 import logged from '../logged'
-// import styles from './styles.module.css'
+import styles from './styles.module.css'
 
-const Wrapper = styled.div`
-  text-align: center;
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: flex-start;
-  font-size: calc(10px + 2vmin);
-  color: white;
-  padding-top: 40px;
+// const Wrapper = styled.div`
+//   text-align: center;
+//   background-color: #282c34;
+//   min-height: 100vh;
+//   display: flex;
+//   flex-direction: row;
+//   flex-wrap: wrap;
+//   justify-content: center;
+//   align-items: flex-start;
+//   font-size: calc(10px + 2vmin);
+//   color: white;
+//   padding-top: 40px;
 
-  ${props => props.red && css`
-    background: red;
-  `}
-`
+//   ${props => props.red && css`
+//     background: red;
+//   `}
+// `
 
 const renderCards = (products) => {
   return products.map((product) => {
@@ -37,23 +37,38 @@ const renderCards = (products) => {
 
 class ProductList extends React.Component {
   state = {
-    isRed: false
+    isRed: false,
+    ownerName: 'Placeholder'
   }
 
   handleClick = () => {
     this.setState({
-      isRed: !this.state.isRed
+      isRed: !this.state.isRed,
     })
   }
 
+  componentDidMount() {
+    fetch('https://api.github.com/users/AtanasKrondev')
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          ownerName: json.name
+        })
+        console.log(json)
+      })
+      .catch((myErr) => console.error(myErr))
+  }
+
   render() {
+    const themeClass = this.state.isRed ? styles.redContainer : styles.container;
     return (
       <Fragment>
         <Header />
+        <div>{this.state.ownerName}</div>
         <button onClick={this.handleClick}>Toggle Red Theme</button>
-        <Wrapper red={this.state.isRed}>
+        <div className={themeClass}>
           {renderCards(this.props.products)}
-        </Wrapper>
+        </div>
       </Fragment >
     )
   }
